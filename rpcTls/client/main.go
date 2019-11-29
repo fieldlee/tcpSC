374c0269e797cda537f7c9033f37a87e0a784eb5
fieldlee <ldpfieldlee@gmail.com>
2019-11-29 18:44:33 +0800

    add tls rpc

---
+++ rpcTls/client/main.go
@@ -0,0 +1,36 @@
+package main
+
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc"
+	"log"
+	"strconv"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func GreatCommonDivisor(first, second string) {
+	// create the client TLS credentials
+	creds, err := credentials.NewClientTLSFromFile("../cert/server.crt", "")
+	// initiate a connection with the server using creds
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewSimpleMathClient(conn)
+	a, _ := strconv.ParseInt(first, 10, 32)
+	b, _ := strconv.ParseInt(second, 10, 32)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GreatCommonDivisor(ctx, &pb.GCDRequest{First: int32(a), Second: int32(b)})
+	if err != nil {
+		log.Fatalf("could not compute: %v", err)
+	}
+	log.Printf("The Greatest Common Divisor of %d and %d is %d", a, b, r.Result)
+}
---
+++ rpcTls/server/main.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50051"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// create the TLS credentials from files
+	creds, err := credentials.NewServerTLSFromFile("../cert/server.crt", "../cert/server.key")
+	if err != nil {
+		log.Fatalf("could not load TLS keys: %s", err)
+	}
+	// create a gRPC option array with the credentials
+	opts := []grpc.ServerOption{grpc.Creds(creds)}
+	// create a gRPC server object with server options(opts)
+	s := grpc.NewServer(opts...)
+	//pb.RegisterSimpleMathServer(s, &rpcimpl.SimpleMathServer{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
